ab7e48b904b5927486b7600235f14a934c97db72
rhyous <rhyous@yahoo.com>
2016-03-08 15:58:36 -0700

    Added Unit Test project

--- src/Unit Tests/Rhyous.EasyXml.Tests/Properties/AssemblyInfo.cs
+++ src/Unit Tests/Rhyous.EasyXml.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rhyous.EasyXml")]
+[assembly: AssemblyTitle("Rhyous.EasyXml.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rhyous.EasyXml")]
+[assembly: AssemblyProduct("Rhyous.EasyXml.Tests")]
 [assembly: AssemblyCopyright("Copyright © Rhyous Publishing LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee497b86-5762-441e-b7a8-4b84df52b723")]
+[assembly: Guid("ea204f8e-2ff1-4e28-b380-1d1a3c99cf23")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ src/Unit Tests/Rhyous.EasyXml.Tests/XmlTests.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace Rhyous.EasyXml.Tests
+{
+    [TestClass]
+    public class XmlTests
+    {
+        public string LinearUtf8Xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Person><FirstName>John</FirstName><MiddleName>Al Leon</MiddleName><LastName>Doe</LastName></Person>";
+        public string PrettyUtf8Xml =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<Person>
+  <FirstName>John</FirstName>
+  <MiddleName>Al Leon</MiddleName>
+  <LastName>Doe</LastName>
+</Person>";
+        public string PrettyUtf8XmlWithTabs =
+@"<?xml version=""1.0"" encoding=""UTF-8""?>
+<Person>
+	<FirstName>John</FirstName>
+	<MiddleName>Al Leon</MiddleName>
+	<LastName>Doe</LastName>
+</Person>";
+        public string UglyUtf8Xml =
+@"<?xml version=""1.0""
+encoding=""UTF-8""?>
+<Person>
+
+<FirstName>
+    John
+        </FirstName>
+
+<MiddleName>
+    Al
+    Leon
+                </MiddleName>
+  <LastName>
+    
+
+
+Doe
+        </LastName>
+
+
+</Person>";
+
+        public string LinearUtf16Xml = "<?xml version=\"1.0\" encoding=\"UTF-16\"?><Person><FirstName>John</FirstName><MiddleName>Al Leon</MiddleName><LastName>Doe</LastName></Person>";
+        public string PrettyUtf16Xml =
+@"<?xml version=""1.0"" encoding=""UTF-16""?>
+<Person>
+  <FirstName>John</FirstName>
+  <MiddleName>Al Leon</MiddleName>
+  <LastName>Doe</LastName>
+</Person>";
+        public string UglyUtf16Xml =
+@"<?xml version=""1.0""
+encoding=""UTF-16""?>
+<Person>
+
+<FirstName>
+    John
+        </FirstName>
+
+<MiddleName>
+    Al
+    Leon
+                </MiddleName>
+  <LastName>
+    
+
+
+Doe
+        </LastName>
+
+
+</Person>";
+
+        [TestMethod]
+        public void TestMethodLinearize()
+        {
+            // Arrange
+            Xml xml = new Xml(PrettyUtf8Xml);
+
+            // Act
+            var actual = xml.LinearizeXml;
+
+            // Assert
+            Assert.AreEqual(LinearUtf8Xml, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodPretty()
+        {
+            // Arrange
+            Xml xml = new Xml(LinearUtf8Xml);
+
+            // Act
+            var actual = xml.PrettyXml;
+
+            // Assert
+            Assert.AreEqual(PrettyUtf8Xml, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodLinearizeUgly()
+        {
+            // Arrange
+            Xml xml = new Xml(UglyUtf8Xml);
+
+            // Act
+            var actual = xml.LinearizeXml;
+
+            // Assert
+            Assert.AreEqual(LinearUtf8Xml, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodMakeUglyPretty()
+        {
+            // Arrange
+            Xml xml = new Xml(UglyUtf8Xml);
+
+            // Act
+            var actual = xml.PrettyXml;
+
+            // Assert
+            Assert.AreEqual(PrettyUtf8Xml, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodLinearizeUglyUtf16()
+        {
+            // Arrange
+            Xml xml = new Xml(UglyUtf16Xml)
+            {
+                Encoding = Xml.XmlEncoding.UTF16
+            };
+
+            // Act
+            var actual = xml.LinearizeXml;
+
+            // Assert
+            Assert.AreEqual(LinearUtf16Xml, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodMakeUglyPrettyUtf16()
+        {
+            // Arrange
+            Xml xml = new Xml(UglyUtf16Xml)
+            {
+                Encoding = Xml.XmlEncoding.UTF16
+            };
+
+            // Act
+            var actual = xml.PrettyXml;
+
+            // Assert
+            Assert.AreEqual(PrettyUtf16Xml, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodStreamIsUtf8()
+        {
+            // Arrange
+            Xml xml = new Xml(UglyUtf8Xml)
+            {
+                Encoding = Xml.XmlEncoding.UTF8
+            };
+
+            // Act
+            var actual = xml.ToStream();
+            using (var memoryStream = new MemoryStream())
+            {
+                actual.CopyTo(memoryStream);
+                var bytes = memoryStream.ToArray();
+                // Assert
+                Assert.AreEqual(154, bytes.Length);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethodStreamIsUtf16()
+        {
+            // Arrange
+            Xml xml = new Xml(UglyUtf16Xml)
+            {
+                Encoding = Xml.XmlEncoding.UTF16
+            };
+
+            // Act
+            var actual = xml.ToStream();
+            using (var memoryStream = new MemoryStream())
+            {
+                actual.CopyTo(memoryStream);
+                var bytes = memoryStream.ToArray();
+
+                // Assert
+                // 310 is twice the size of 154, 308, but add 2 bytes because
+                // UTF-8 is 5 characters but UTF-16 is 6 characters so it is 
+                // one character longer.John
+                Assert.AreEqual(310, bytes.Length);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethodPrettyWuthTabs()
+        {
+            // Arrange
+            Xml xml = new Xml(LinearUtf8Xml)
+            {
+                IndentCharacters = "\t"
+            };
+
+            // Act
+            var actual = xml.PrettyXml;
+
+            // Assert
+            Assert.AreEqual(PrettyUtf8XmlWithTabs, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodStreamUtf8IsDifferentThanStreamUtf16()
+        {
+            const string text = "Hello, world!";
+
+            var utf8 = Encoding.UTF8.GetBytes(text);
+            var utf16 = Encoding.Unicode.GetBytes(text);
+
+            Assert.AreNotEqual(utf8.Length, utf16.Length);
+        }
+    }
+}
