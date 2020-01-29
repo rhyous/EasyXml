11c4e2e4a6d216d73807c68cb8797a8287a5568e
Jared A Barneck <rhyous@yahoo.com>
2020-01-29 11:55:29 -0700

    Moved to .Net Standard and new package type with AppVeyor building NuGet

---
+++ src/Rhyous.EasyXml.Tests/SerializableDictionaryTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rhyous.EasyXml.Tests
+{
+    [TestClass]
+    public class SerializableDictionaryTests
+    {
+        [TestMethod]
+        public void TestSerializeStringString()
+        {
+            var expected = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value>one</value><key>2</key><value>two</value></Dictionary>";
+            var dict = new SerializableDictionary<string, string>();
+            dict.Add("1", "one");
+            dict.Add("2", "two");
+            var actual = Serializer.SerializeToXml(dict);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDeserializeStringString()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value>one</value><key>2</key><value>two</value></Dictionary>";
+            var actual = Serializer.DeserializeFromXml<SerializableDictionary<string, string>>(ref xml);
+            Assert.AreEqual("one", actual["1"]);
+        }
+
+        [TestMethod]
+        public void TestSerializeIntString()
+        {
+            var expected = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value>one</value><key>2</key><value>two</value></Dictionary>";
+            var dict = new SerializableDictionary<int, string>();
+            dict.Add(1, "one");
+            dict.Add(2, "two");
+            var actual = Serializer.SerializeToXml(dict);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDeserializeIntString()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value>one</value><key>2</key><value>two</value></Dictionary>";
+            var actual = Serializer.DeserializeFromXml<SerializableDictionary<int, string>>(ref xml);
+            Assert.AreEqual("one", actual[1]);
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        [TestMethod]
+        public void TestSerializeIntPerson()
+        {
+            var expected = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value><FirstName>Joe</FirstName><LastName>Johnson</LastName></value><key>2</key><value><FirstName>Tom</FirstName><LastName>Tomison</LastName></value></Dictionary>";
+            var dict = new SerializableDictionary<int, Person>();
+            dict.Add(1, new Person { FirstName = "Joe", LastName = "Johnson"});
+            dict.Add(2, new Person { FirstName = "Tom", LastName = "Tomison" });
+            var actual = Serializer.SerializeToXml(dict);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDeserializeIntPerson()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value><FirstName>Joe</FirstName><LastName>Johnson</LastName></value><key>2</key><value><FirstName>Tom</FirstName><LastName>Tomison</LastName></value></Dictionary>";
+            var actual = Serializer.DeserializeFromXml<SerializableDictionary<int, Person>>(ref xml);
+            Assert.AreEqual("Joe", actual[1].FirstName);
+            Assert.AreEqual("Johnson", actual[1].LastName);
+            Assert.AreEqual("Tom", actual[2].FirstName);
+            Assert.AreEqual("Tomison", actual[2].LastName);
+        }
+
+        public class NoEmptyConstructor
+        {
+            public NoEmptyConstructor(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void TestSerializeIntNoEmptyConstructor()
+        {
+            var dict = new SerializableDictionary<int, NoEmptyConstructor>();
+            dict.Add(1, new NoEmptyConstructor("Joe","Johnson"));
+            dict.Add(2, new NoEmptyConstructor("Tom", "Tomison"));
+            var actual = Serializer.SerializeToXml(dict);            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void TestDeserializeIntNoEmptyConstructor()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Dictionary><key>1</key><value><FirstName>Joe</FirstName><LastName>Johnson</LastName></value><key>2</key><value><FirstName>Tom</FirstName><LastName>Tomison</LastName></value></Dictionary>";
+            var actual = Serializer.DeserializeFromXml<SerializableDictionary<int, NoEmptyConstructor>>(ref xml);
+        }
+    }
+}
---
+++ src/Rhyous.EasyXml.Tests/SerializerTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Rhyous.EasyXml.Tests
+{
+    public class A
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<B> Bs { get; set; }
+    }
+    public class B
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    [TestClass]
+    public class SerializerTests
+    {
+        [TestMethod]
+        public void OnlyOneParameterNeededToCreateString()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+
+            // Act
+            Serializer.SerializeToXml(a);
+
+            // Assert
+            // Just make sure this runs.
+        }
+
+        [TestMethod]
+        public void TwoParametesToCreateStringWorks()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+
+            // Act
+            Serializer.SerializeToXml(a, true);
+
+            // Assert
+            // Just make sure this runs.
+        }
+
+        [TestMethod]
+        public void ThreeParametesToCreateStringWorks()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+
+            // Act
+            Serializer.SerializeToXml(a, true, null);
+
+            // Assert
+            // Just make sure this runs.
+        }
+
+        [TestMethod]
+        public void OnlyTwoParameterNeededToCreateFile()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+
+            // Act
+            Serializer.SerializeToXml(a, "file.xml");
+
+            // Assert
+            // Just make sure this runs.
+        }
+
+        [TestMethod]
+        public void MethodWithThreeParametersWorks()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+
+            // Act
+            Serializer.SerializeToXml(a, "file.xml", true);
+
+            // Assert
+            // Just make sure this runs.
+        }
+
+        [TestMethod]
+        public void MethodWithFourParametersWorks()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+
+            // Act
+            Serializer.SerializeToXml(a, true, new XmlSerializerNamespaces(), Encoding.UTF8);
+
+            // Assert
+            // Just make sure this runs.
+        }
+
+        [TestMethod]
+        public void UseDefaultNamespacesTests()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+            var expectedXml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><A xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><Id>1</Id><Name>A1</Name><Bs><B><Id>1</Id><Name>B1</Name></B><B><Id>1</Id><Name>B2</Name></B></Bs></A>";
+
+            // Act
+            var xml = Serializer.Instance.ToXml(a, false, null, Encoding.UTF8, true);
+
+            // Assert
+            Assert.AreEqual(expectedXml, xml);
+        }
+
+        [TestMethod]
+        public void UseDefaultNamespacesStaticTests()
+        {
+            // Arrange
+            var a = new A { Id = 1, Name = "A1", Bs = new List<B> { new B { Id = 1, Name = "B1" }, new B { Id = 1, Name = "B2" } } };
+            var expectedXml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><A xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><Id>1</Id><Name>A1</Name><Bs><B><Id>1</Id><Name>B1</Name></B><B><Id>1</Id><Name>B2</Name></B></Bs></A>";
+
+            // Act
+            var xml = Serializer.SerializeToXml(a, false, null, Encoding.UTF8, true);
+
+            // Assert
+            Assert.AreEqual(expectedXml, xml);
+        }
+    }
+}
